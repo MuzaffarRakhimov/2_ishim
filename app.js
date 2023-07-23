7ac504bb79f4df50b745250d94caeef7354f7463
MuzaffarRaximov <javascriptmernstack@gmail.com>
2023-07-24 01:10:55 +0300

    bb

---
+++ app.js
@@ -0,0 +1,33 @@
+const oyna = document.getElementById("m");
+const yoz = document.getElementById("yoz");
+const pp = document.getElementById("pp");
+
+function ozgar(o) {
+  oyna.src = o;
+};
+
+function h(b) {
+  yoz.innerHTML = b;
+
+//   if(b=='Html')
+//   {
+//    pp.innerHTML="bu html";
+//   }
+//   else if(b=='Css')
+//   {
+//    pp.innerHTML='bu css';
+//   }
+//   else if(b=='JavaScript')
+//   {
+//    pp.innerHTML='bu JavaScript';
+//   }
+//   else (b=='React')
+//   {
+//    pp.innerHTML='bu React';
+//   };
+};
+
+function l(p){
+   pp.innerHTML=p;
+
+}
